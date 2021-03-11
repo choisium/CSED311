3d422f08054fd527a67b4a204ffb16ca4db77667
Soomin Choi <choisium@gmail.com>
2021-03-11 15:02:15 +0900

    lab2: add combinational logic to calculate current state

--- lab2/calculate_current_state.v
+++ lab2/calculate_current_state.v
@@ -3,7 +3,7 @@
 	
 
 module calculate_current_state(i_input_coin,i_select_item,item_price,coin_value,current_total,
-input_total, output_total, return_total,current_total_nxt,wait_time,o_return_coin,o_available_item,o_output_item);
+current_total_nxt,wait_time,o_return_coin,o_available_item,o_output_item);
 
 
 	
@@ -14,27 +14,43 @@ input_total, output_total, return_total,current_total_nxt,wait_time,o_return_coi
 	input [`kTotalBits-1:0] current_total;
 	input [31:0] wait_time;
 	output reg [`kNumItems-1:0] o_available_item,o_output_item;
-	output reg  [`kTotalBits-1:0] input_total, output_total, return_total,current_total_nxt;
+	output reg  [`kTotalBits-1:0] current_total_nxt;
 	integer i;	
 
-
+	wire [`kTotalBits-1:0] inserted_coin, selected_item;
+	assign inserted_coin = i_input_coin[0] * coin_value[0]
+						+ i_input_coin[1] * coin_value[1]
+						+ i_input_coin[2] * coin_value[2];
+	assign selected_item = i_select_item[0] * item_price[0]
+						+ i_select_item[1] * item_price[1]
+						+ i_select_item[2] * item_price[2]
+						+ i_select_item[3] * item_price[3];
 
 	
 	// Combinational logic for the next states
 	always @(*) begin
-		// TODO: current_total_nxt
-		// You don't have to worry about concurrent activations in each input vector (or array).
-		// Calculate the next current_total state.
-		
-	end
+		// when item is selected and it is available
+		if (selected_item !== 0 && selected_item <= current_total) begin
+			current_total_nxt = current_total - selected_item;
+			o_output_item = i_select_item;
+		end
+		// when coin is inserted or item is selected but unavailable
+		else begin
+			current_total_nxt = current_total + inserted_coin;
+			o_output_item = `kNumItems'b0;
+		end
 
-	
-	
-	// Combinational logic for the outputs
-	always @(*) begin
-		// TODO: o_available_item
-		// TODO: o_output_item
+		// check available items using current_total_nxt
+		for (i = 0; i < `kNumItems; i = i + 1) begin
+			if (item_price[i] <= current_total_nxt)
+				o_available_item[i] = 1;
+			else
+				o_available_item[i] = 0;
+		end
 
+		// print available items
+		if (inserted_coin || o_output_item)
+			$strobe("o_available_item = %0b", o_available_item);
 	end
  
 	
