011752cb84ca470e50a73ff355596824b1842256
Soomin Choi <choisium@gmail.com>
2021-03-17 23:20:33 +0900

    lab2: add control units and memory_access module

--- lab3/control_unit.v
+++ lab3/control_unit.v
@@ -1,6 +1,6 @@
 `include "opcodes.v" 	   
 
-module control_unit (instr, alu_src, reg_write, mem_read, mem_to_reg, mem_write, jp, branch);
+module control_unit (instr, alu_src, reg_write, mem_read, mem_to_reg, mem_write, jp, branch, pc_to_reg, rt_write, zero_extended);
 input [`WORD_SIZE-1:0] instr;
 output reg alu_src;
 output reg reg_write;
@@ -9,5 +9,103 @@ output reg mem_to_reg;
 output reg mem_write;
 output reg jp;
 output reg branch;
+output reg pc_to_reg;
+output reg rt_write;
+output reg zero_extended;
+
+wire [3:0] opcode;
+assign opcode = instr[15:12];
+wire [5:0] func_code;
+assign func_code = instr[5:0];
+
+
+always @(*) begin
+    // reset each control value;s
+    alu_src = 0;
+    reg_write = 0;
+    mem_read = 0;
+    mem_to_reg = 0;
+    mem_write = 0;
+    jp = 0;
+    branch = 0;
+    pc_to_reg = 0;
+    rt_write = 0;
+    zero_extended = 0;
+    case (opcode)
+        `ALU_OP, `JPR_OP, `JRL_OP: begin     // R-type Instructions
+            case (func_code)
+                `INST_FUNC_JPR: begin        // JPR
+                    reg_write = 0;
+                    jp = 1;
+                    pc_to_reg = 0;
+                end
+                `INST_FUNC_JRL: begin        // JRL
+                    reg_write = 0;
+                    jp = 1;
+                    pc_to_reg = 0;
+                end
+                default: begin               // ALU operations
+                    reg_write = 1;
+                    jp = 0;
+                    pc_to_reg = 0;
+                end
+            endcase
+        end
+        `ADI_OP, `LHI_OP: begin
+            alu_src = 1;
+            reg_write = 1;
+            rt_write = 1;
+        end
+        `ORI_OP: begin
+            alu_src = 1;
+            reg_write = 1;
+            rt_write = 1;
+            zero_extended = 1;
+        end
+        `LWD_OP: begin
+            alu_src = 1;
+            reg_write = 1;
+            rt_write = 1;
+            mem_read = 1;
+            mem_to_reg = 1;
+        end
+        `SWD_OP: begin
+            alu_src = 1;
+            mem_write = 1;
+        end
+        `BNE_OP, `BEQ_OP: begin
+            branch = 1;
+        end
+        `BGZ_OP, `BLZ_OP: begin
+            alu_src = 1;
+            branch = 1;
+        end
+        `JMP_OP: begin
+            jp = 1;
+        end
+        `JAL_OP: begin
+            jp = 1;
+            pc_to_reg = 1;
+        end
+
+        default: begin
+                alu_src = 0;
+                reg_write = 0;
+                mem_read = 0;
+                mem_to_reg = 0;
+                mem_write = 0;
+                jp = 0;
+                branch = 0;
+                pc_to_reg = 0;
+                rt_write = 0;
+	    end
+    endcase
+
+
+    $display("opcode: %d", opcode);
+    $display("alu_src: %d, reg_write: %d, mem_read: %d, mem_to_reg: %d, mem_write: %d, jp: %d, branch: %d, pc_to_reg: %d, rt_write: %d",
+            alu_src, reg_write, mem_read, mem_to_reg, mem_write, jp, branch, pc_to_reg, rt_write);
+end
+
 
 endmodule
---
+++ lab3/memory_access.v
@@ -0,0 +1,52 @@
+`include "opcodes.v" 	   
+
+module memory_access (pc, pc_nxt, mem_read, mem_write, mem_address, mem_data,
+					  readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
+	output reg[5:0] pc, pc_nxt;
+	output reg readM;
+	output reg writeM;
+	output reg [`WORD_SIZE-1:0] address;
+	inout [`WORD_SIZE-1:0] data;
+	input mem_read;
+	input mem_write;
+	input [`WORD_SIZE-1:0] mem_address;
+	input [`WORD_SIZE-1:0] mem_data;
+	input ackOutput;
+	input inputReady;
+	input reset_n;
+	input clk;
+
+	initial begin
+		pc = 0;
+		pc_nxt = 0;
+	end
+
+	reg temp_data;
+
+	assign data = (readM || inputReady)? `WORD_SIZE'bz: temp_data;
+
+	// update pc
+	always @(posedge clk) begin
+		pc_nxt <= pc_nxt + 1;
+	end
+
+	// instruction fetch
+	always @(posedge clk) begin
+		pc <= pc_nxt;
+		readM <= 1;
+		writeM <= 0;
+		address <= pc;
+		temp_data <= `WORD_SIZE'bz;
+		$strobe("pc value: %d, pc nxt value: %d", pc, pc_nxt);
+		$strobe("readM: %d, writeM: %d, address: %d", readM, writeM, address);
+	end
+
+	// read or write memory
+	always @(negedge clk) begin
+		readM <= mem_read;
+		writeM <= mem_write;
+		address <= mem_address;
+		temp_data <= mem_read ? `WORD_SIZE'bz : mem_data;
+	end
+
+endmodule
