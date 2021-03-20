e9ae76795746d9bd66c6dd13bd70117e2c6272c4
Gyumin Choi <kyuchoi95@postech.ac.kr>
2021-03-20 18:12:06 +0900

    lab3: add register_file and alu modules

--- lab3/alu.v
+++ lab3/alu.v
@@ -2,10 +2,26 @@
 
 `define	NumBits	16
 
-module alu (alu_input_1, alu_input_2, func_code, alu_output);
+module alu (alu_input_1, alu_input_2, alu_func_code, alu_output, zero);
 	input [`NumBits-1:0] alu_input_1;
 	input [`NumBits-1:0] alu_input_2;
-	input [2:0] func_code;
+	input [3:0] alu_func_code;
 	output reg [`NumBits-1:0] alu_output;
+	output zero;
 
+	assign zero = (alu_output == 0);
+
+	always @(*) begin
+		case(alu_func_code)
+			`FUNC_ADD: alu_output = alu_input_1 + alu_input_2;
+			`FUNC_SUB: alu_output = alu_input_1 - alu_input_2;
+			`FUNC_AND: alu_output = alu_input_1 & alu_input_2;
+			`FUNC_ORR: alu_output = alu_input_1 | alu_input_2;
+			`FUNC_NOT: alu_output = ~alu_input_1;
+			`FUNC_TCP: alu_output = ~alu_input_1 + 1;
+			`FUNC_SHL: alu_output = {alu_input_1[14:0], 1'b0};
+			`FUNC_SHR: alu_output = {alu_input_1[15], alu_input_1[15:1]} ;
+			default: alu_output = 0; // should not happen
+		endcase
+	end
 endmodule
--- lab3/register_file.v
+++ lab3/register_file.v
@@ -8,5 +8,13 @@ module register_file( read_out1, read_out2, read1, read2, write_reg, write_data,
     input reg_write;
     input clk;
 
-endmodule
+    reg [3:0] RF [15:0]; // 4 registers each 16 bits long
 
+    assign read_out1 = RF[read1];
+    assign read_out2 = RF[read2];
+
+    always @(posedge clk) begin
+        // write back if reg_write is high
+        if (reg_write) RF[write_reg] <= write_data; 
+    end
+endmodule
