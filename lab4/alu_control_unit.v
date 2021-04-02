d648febe1a9d8315d4b29cf812dbc60f45f794be
Gyumin Choi <kyuchoi95@postech.ac.kr>
2021-04-02 10:09:46 +0900

    lab4: add control units

--- lab4/alu_control_unit.v
+++ lab4/alu_control_unit.v
@@ -11,4 +11,31 @@ module alu_control_unit(funct, opcode, ALUOp, clk, funcCode, branchType);
 
    //TODO: implement ALU control unit
   
+always @(posedge clk) begin
+    if(!ALUOp) begin // common operation
+        funcCode <= `FUNC_ADD;
+        branchType <= 0;
+    end
+    else begin // individual operations
+       case (opcode)
+            `ALU_OP: begin
+                case(funct)
+                    `INST_FUNC_JPR: funcCode <= `FUNC_ID1; // pc <- rs
+                    `INST_FUNC_JRL: funcCode <= `FUNC_ID1; // pc <- rs
+                    `INST_FUNC_WWD: funcCode <= `FUNC_ID1; // outputport <- rs
+                    default: funcCode <= funct[3:0];
+                endcase
+            end
+            `ADI_OP: funcCode <= `FUNC_ADD;
+            `ORI_OP: funcCode <= `FUNC_ORR;
+            `LHI_OP: funcCode <= `FUNC_ID2; // immediate : alu_input_2
+            `BNE_OP, `BEQ_OP ,`BGZ_OP, `BLZ_OP: begin 
+                funcCode <= `FUNC_Bxx;
+                branchType <= opcode[1:0]; //branch type for bne = 0, beq = 1, bgz = 2, blz = 3
+            end
+            default: funcCode <= 4'd15; // no use
+        endcase 
+    end
+end
+
 endmodule
--- lab4/control_unit.v
+++ lab4/control_unit.v
@@ -11,8 +11,208 @@ module control_unit(opcode, func_code, clk, pc_write_cond, pc_write, i_or_d, mem
   output reg [1:0] reg_write, alu_src_A, alu_src_B;
   output reg alu_op;
 
+  // state register
+  reg [4:0] current_state, next_state;
 
-   //TODO: implement control unit
+  // localparam for states
+  localparam IF = 5d'0;
+  localparam ID = 5d'1;
+  localparam R_EX = 5d'2;
+  localparam R_WB = 5d'3;
+  localparam I_EX = 5d'4;
+  localparam I_WB = 5d'5;
+  localparam MEM_EX = 5d'6;
+  localparam LD_MEM = 5d'7;
+  localparam LD_WB = 5d'8;
+  localparam SD_MEM = 5d'9;
+  localparam BCheck = 5d'10;
+  localparam BComplete = 5d'11;
+  localparam JAL = 5d'12;
+  localparam JMP = 5d'13;
+  localparam JRL = 5d'14;
+  localparam JPR = 5d'15;
+  localparam WWD = 5d'16;
+  localparam HLT = 5d'17;
 
+  initial begin
+    current_state <= 0;
+    next_state <= 0;
+  end
+
+  // update state 
+  always @(posedge clk) begin
+    current_state <= next_state;
+  end
+
+  // logic for next state
+  always @(posedge clk) begin
+    case(current_state)
+        IF: next_state <= ID;
+        ID: begin
+          case(opcode) 
+              4'd15: begin // R-Type
+                case(func_code)
+                  `INST_FUNC_JPR: next_state <= JPR;
+                  `INST_FUNC_JRL: next_state <= JRL;
+                  `INST_FUNC_WWD: next_state <= WWD;
+                  `INST_FUNC_HLT: next_state <= HLT;
+                  default: next_state <= R_EX;
+                endcase
+            `ADI_OP, `ORI_OP, `LHI_OP: next_state <= I_EX;
+            `LWD_OP, `SWD_OP: next_state <= MEM_EX;
+            `BNE_OP, `BEQ_OP, `BGZ_OP, `BLZ_OP: next_state <= BCheck;
+            `JAL_OP: next_state <= JAL;
+            `JMP_OP: next_state <= JMP;
+            default: next_state <= HLT; // no use; exception
+          endcase
+        end
+        R_EX: next_state <= R_WB;
+        I_EX: next_state <= I_WB;
+        MEM_EX: begin
+          case(opcode) 
+            `LWD_OP: next_state <= LD_MEM;
+            `SWD_OP: next_state <= SD_MEM;
+          endcase
+        end
+        LD_MEM: next_state <= LD_WB;
+        BCheck: next_state <= BComplete;
+        JAL: next_state <= JMP;
+        JRL: next_state <= JPR;
+        default: next_state <= IF; // leaf node states -> IF
+    endcase
+  end
+
+  // control signal for current state
+  always @(posedge clk) begin
+    
+    // reset each control values
+    pc_write_cond <= 0;
+    pc_write <= 0;
+    i_or_d <= 0;
+    mem_read <= 0;
+    mem_to_reg <= 0;
+    mem_write <= 0;
+    ir_write <= 0;
+    pc_src <= 0;
+    pc_to_reg <= 0;
+    halt <= 0;
+    wwd <= 0;
+    new_inst <= 0;
+    reg_write <= 2b'00;
+    alu_src_A <= 2b'00;
+    alu_src_B <= 2b'00;
+    alu_op <= 0;
+    
+    case (current_state) 
+      
+      IF: begin
+        mem_read <= 1;
+        i_or_d <= 0;
+        ir_write <= 1;
+        pc_write <= 1;
+        alu_src_A <= 2b'00;
+        alu_src_B <= 2b'01;
+        alu_op <= 0;
+        new_inst <= 1;
+      end
+      
+      ID: begin
+        alu_src_A <= 2b'00;
+        alu_src_B <= 2b'11;
+        alu_op <= 0;
+      end
+      
+      R_EX: begin
+        alu_src_A <= 2b'01;
+        alu_src_B <= 2b'00;
+        alu_op <= 1;
+      end
+      
+      R_WB: begin
+        reg_write <= 2b'01;
+        mem_to_reg <= 0;
+      end
+      
+      I_EX: begin
+        alu_src_A <= 2b'01;
+        alu_src_B <= 2b'10;
+        alu_op <= 1;
+      end
+      
+      I_WB: begin
+        reg_write <= 2b'10;
+        mem_to_reg <= 0;
+      end
+      
+      MEM_EX: begin
+        alu_src_A <= 2b'01;
+        alu_src_B <= 2b'10;
+        alu_op <= 0;
+      end
+
+      LD_MEM: begin
+        mem_read <= 1;
+        i_or_d <= 1;
+      end
+
+      LD_WB: begin
+        reg_write <= 2b'10;
+        mem_to_reg <= 1;
+      end
+
+      SD_MEM: begin
+        mem_write <= 1;
+        i_or_d <= 1;
+      end
+
+      BCheck: begin
+        alu_src_A <= 2b'01;
+        alu_src_B <= 2b'00;
+        alu_op <= 1; 
+      end
+
+      BComplete: begin
+        alu_src_A <= 2b'00;
+        alu_src_B <= 2b'10;
+        pc_write_cond <= 1;
+        pc_src <= 0;
+        alu_op <= 0;
+      end
+
+      JAL: begin
+        pc_to_reg <= 1;
+        reg_wrtie <= 2b'11;
+      end
+
+      JMP: begin
+        pc_write <= 1;
+        pc_src <= 1;
+      end
+
+      JRL: begin
+        pc_to_reg <= 1;
+        reg_write <= 2b'11;
+      end
+
+      JPR: begin
+        alu_src_A <= 2b'01;
+        alu_src_B <= 2b'11;
+        pc_write <= 1;
+        pc_src <= 0;
+        alu_op <= 1;
+      end
+
+      WWD: begin
+        wwd <= 1;
+        alu_src_A <= 2b'01;
+        alu_src_B <= 2b'11;
+        alu_op <= 1;
+      end
+
+      default: begin
+        halt <= 1;
+      end
+    endcase
+  end
 
 endmodule
