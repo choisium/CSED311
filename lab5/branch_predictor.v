c8fffea17343aabdbdaa43839429ea6af7c6d48b
Soomin Choi <choisium@gmail.com>
2021-04-27 19:37:34 +0900

    lab5: modify always taken branch predictor to store always taken result

--- lab5/branch_predictor.v
+++ lab5/branch_predictor.v
@@ -17,14 +17,15 @@ module branch_predictor_always_not_taken(clk, reset_n, PC, is_flush, is_BJ_type,
 endmodule
 
 
-module branch_predictor_always_taken(clk, reset_n, PC, is_flush, is_BJ_type, actual_next_PC, actual_PC, next_PC);
+module branch_predictor_always_taken(clk, reset_n, PC, is_flush, is_BJ_type, actual_taken_PC, actual_next_PC, actual_PC, next_PC);
 
 	input clk;
 	input reset_n;
 	input [`WORD_SIZE-1:0] PC;
 	input is_flush;
 	input is_BJ_type;
-	input [`WORD_SIZE-1:0] actual_next_PC; //computed actual next PC from branch resolve stage
+	input [`WORD_SIZE-1:0] actual_taken_PC; // PC in case of always taken from branch resolve stage
+	input [`WORD_SIZE-1:0] actual_next_PC; // computed actual next PC from branch resolve stage
 	input [`WORD_SIZE-1:0] actual_PC; // PC from branch resolve stage
 
 	output reg [`WORD_SIZE-1:0] next_PC;
@@ -37,7 +38,7 @@ module branch_predictor_always_taken(clk, reset_n, PC, is_flush, is_BJ_type, act
 	assign tag = PC[`WORD_SIZE-1:`IDX_SIZE];
 	assign idx = PC[`IDX_SIZE-1:0];
 
-	always @(*) begin
+	always @(posedge clk) begin
 		if (!reset_n) begin
 			for(i = 0; i < `IDX_SIZE; i=i+1) begin
 				tagtable[i] = ~0;
@@ -45,23 +46,27 @@ module branch_predictor_always_taken(clk, reset_n, PC, is_flush, is_BJ_type, act
 			end
 		end else begin
 			$display("tagtable: %b, btb: %b", tagtable[idx], btb[idx]);
-			$display("idx: %b, tag: %b", idx, tag);
-			if (is_flush) begin
-				$display("prediction failed! update table");
-				next_PC = actual_next_PC;
+			$display("idx: %b, tag: %b, is_BJ_type: %b", idx, tag, is_BJ_type);
+			if (is_BJ_type) begin
+				$display("Update table for BJ type! actual_next_pc: %h, actual_taken_pc: %h", actual_next_PC, actual_taken_PC);
 				tagtable[actual_PC[`IDX_SIZE-1:0]] = actual_PC[`WORD_SIZE-1:`IDX_SIZE];
-				btb[actual_PC[`IDX_SIZE-1:0]] = actual_next_PC;
-			end else if (tagtable[idx] == tag) begin
-				$display("tag is same! actual_pc: %h, actual_next_pc: %h", actual_PC, actual_next_PC);
-				next_PC = btb[idx];
-			end else begin
-				$display("tag is not same! actual_pc: %h, actual_next_pc: %h", actual_PC, actual_next_PC);
-				next_PC = !(PC < 16'hc6)? PC: PC + 1;
+				btb[actual_PC[`IDX_SIZE-1:0]] = actual_taken_PC;
 			end
 		end
 	end
 
-	//TODO: implement branch predictor
+	always @(*) begin
+		if (is_flush) begin
+			$display("Prediction is wrong!", actual_next_PC, actual_taken_PC);
+			next_PC = actual_next_PC;
+		end else if (tagtable[idx] == tag) begin
+			$display("tag is same! actual_pc: %h, actual_next_pc: %h", actual_PC, actual_next_PC);
+			next_PC = btb[idx];
+		end else begin
+			$display("tag is not same! actual_pc: %h, actual_next_pc: %h", actual_PC, actual_next_PC);
+			next_PC = !(PC < 16'hc6)? PC: PC + 1;
+		end
+	end
 
 endmodule
 
@@ -78,7 +83,7 @@ module branch_predictor_global_predictor(clk, reset_n, PC, is_flush, is_BJ_type,
 
 	output [`WORD_SIZE-1:0] next_PC;
 
-	assign next_PC = (is_flush | !(PC < 16'hc6))? actual_PC: PC + 1;
+	assign next_PC = (is_flush | !(PC < 16'hc6))? actual_next_PC: PC + 1;
 	//TODO: implement branch predictor
 
 endmodule
