c67f23ed4110ccbeead4b8ba725691be253a5fda
Gyumin Choi <kyuchoi95@postech.ac.kr>
2021-05-18 20:17:20 +0900

    lab6_cache: separate cache tag, data module

--- lab6_with_cache/cache.v
+++ lab6_with_cache/cache.v
@@ -1,56 +1,6 @@
 `include "opcodes.v"
 `include "cache_def.v"
-
-/*cache: data memory, single port, 4 blocks*/
-module dm_cache_data(clk, data_req, data_write, data_read);
-    
-    input clk;
-    input [`CACHE_REQ_SIZE-1:0] data_req;      // data request/command, e.g. RW, valid
-    input [`CACHE_DATA_SIZE-1:0] data_write;   // write port
-    output [`CACHE_DATA_SIZE-1:0] data_read;   // read port
-
-    reg [`CACHE_DATA_SIZE-1:0] data_mem[3:0];
-
-    initial begin
-        data_mem[0] = 0;
-        data_mem[1] = 0;
-        data_mem[2] = 0;
-        data_mem[3] = 0;
-    end
-
-    assign data_read = data_mem[data_req[`CACHE_REQ_INDEX]];
-    
-    always @(posedge clk) begin
-        if (data_req[`CACHE_REQ_WE])
-            data_mem[data_req[`CACHE_REQ_INDEX]] <= data_write;
-    end
-endmodule
-
-/*cache: tag memory, single port, 4 blocks*/
-module dm_cache_tag(clk, tag_req, tag_write, tag_read);
-
-    input clk;
-    input [`CACHE_REQ_SIZE-1:0] tag_req;      //tag request
-    input [`CACHE_TAG_SIZE-1:0] tag_write;    //tag write data
-    output [`CACHE_TAG_SIZE-1:0] tag_read;     //tag read result
-
-    reg [`CACHE_TAG_SIZE-1:0] tag_mem[3:0];
-
-    initial begin
-        tag_mem[0] = 0;
-        tag_mem[1] = 0;
-        tag_mem[2] = 0;
-        tag_mem[3] = 0;
-    end
-    
-    assign tag_read = tag_mem[tag_req[`CACHE_REQ_INDEX]];
-    
-    always @(posedge clk) begin
-        if (tag_req[`CACHE_REQ_WE])
-            tag_mem[tag_req[`CACHE_REQ_INDEX]] <= tag_write;
-    end
-endmodule
-
+`include "cache_module.v"
 
 module cache(clk, reset_n, cpu_read_m1, cpu_address1, cpu_data1, cpu_inputReady1, 
         cpu_read_m2, cpu_write_m2, cpu_address2, cpu_data2, cpu_inputReady2, cpu_ackOutput2,
--- lab6_with_cache/cache_def.v
+++ lab6_with_cache/cache_def.v
@@ -18,36 +18,6 @@
 `define WORD_IDX 3:2  // 2-bit index
 `define WORD_BO 1:0   // 2-bit block offset
 
-// data structures for CPU<->Cache controller interface
-
-// CPU request (CPU->cache controller)
-`define CPU_REQ_SIZE 34
-`define CPU_REQ_VALID 33        // valid request
-`define CPU_REQ_RW 32           // request type : 0 = read, 1 = write
-`define CPU_REQ_ADDR 31:16      // 16-bit request addr
-`define CPU_REQ_DATA 15:0       // 16-bit request data (used when write)
-
-// memory request (cache controller->memory)
-`define MEM_REQ_SIZE  82
-`define MEM_REQ_VALID 81        // valid request
-`define MEM_REQ_RW    80        // request type : 0 = read, 1 = write
-`define MEM_REQ_ADDR 79:64      // 16-bit request addr
-`define MEM_REQ_DATA 63:0       // 64-bit(4 word) request data (used when write)
-
-// data structures for cache controller<->memory interface
-
-// Cache result (cache controller->cpu)
-`define CPU_RES_SIZE 18
-`define CPU_RES_READY 17        // input ready
-`define CPU_RES_ACK 16          // ackOutput
-`define CPU_RES_DATA 15:0       // 16-bit data
-
-// memory controller response (memory -> cache controller)
-`define MEM_DATA_SIZE 66
-`define MEM_DATA_READY 65        // input ready
-`define MEM_DATA_ACK 64          // ackOutput
-`define MEM_DATA 63:0            // 64-bit read back data
-
 // cache line : 4 word
 `define BLOCK_WORD_1 63:48
 `define BLOCK_WORD_2 47:32
